770a16cd61cb48956d17722914994e899abbfc30
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2023-12-19 00:01:28 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-// Charger la navbar
-var navbarContainer = document.getElementById("navbarbox");
-var navbarXhr = new XMLHttpRequest();
-navbarXhr.open("GET", "../htmlelements/navbar.html", true);
-navbarXhr.onreadystatechange = function () {
-    if (navbarXhr.readyState === 4 && navbarXhr.status === 200) {
-        navbarContainer.innerHTML = navbarXhr.responseText;
-    }
-};
-navbarXhr.send();
-
-// Charger le footer
-var footerContainer = document.getElementById("footerbox");
-var footerXhr = new XMLHttpRequest();
-footerXhr.open("GET", "../htmlelements/footer.html", true);
-footerXhr.onreadystatechange = function () {
-    if (footerXhr.readyState === 4 && footerXhr.status === 200) {
-        footerContainer.innerHTML = footerXhr.responseText;
-    }
-};
-footerXhr.send();
 
 
 // FOOTER SUR MOBILE
